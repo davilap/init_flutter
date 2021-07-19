73269a785aec1f671a6a9dc8a22a8d179c8ebeec
Donovan Avila <donovan.avila@gmail.com>
2021-07-19 09:49:48 -0500

    first commit

---
+++ lib/components/no_account.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
+
+import '../constants.dart';
+import '../size_config.dart';
+
+class NoAccountText extends StatelessWidget {
+  const NoAccountText({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Don't have account?",
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(16),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            Navigator.popAndPushNamed(context, ForgotPasswordScreen.routeName);
+          },
+          child: Text(
+            "Sign Up",
+            style: TextStyle(
+                fontSize: getProportionateScreenWidth(16),
+                color: kPrimaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/screens/sign_in/components/sign_in_form.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/components/custom_surffix_icon.dart';
+import 'package:shop_app/components/default_button.dart';
+import 'package:shop_app/components/form_error.dart';
+import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';
+import 'package:shop_app/screens/login_success/login_success_screen.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class SignForm extends StatefulWidget {
+  @override
+  _SignFormState createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  final _formKey = GlobalKey<FormState>();
+  final List<String> errors = [];
+
+  String email;
+  String password;
+  bool remember = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          buildEmailFormField(),
+          SizedBox(
+            height: getProportionateScreenHeight(30),
+          ),
+          buildPasswordFormField(),
+          SizedBox(
+            height: getProportionateScreenHeight(30),
+          ),
+          Row(
+            children: [
+              Checkbox(
+                value: remember,
+                activeColor: kPrimaryColor,
+                onChanged: (value) {
+                  setState(() {
+                    remember = value;
+                  });
+                },
+              ),
+              Text("Remember me"),
+              Spacer(),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, ForgotPasswordScreen.routeName);
+                },
+                child: Text(
+                  "Forgot password",
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            height: getProportionateScreenHeight(20),
+          ),
+          FormError(errors: errors),
+          SizedBox(
+            height: getProportionateScreenHeight(20),
+          ),
+          DefaultButton(
+            text: "Continue",
+            press: () {
+              //validate, is present an error the moment when double click in button continue, its not validate fields
+              if (_formKey.currentState.validate()) {
+                _formKey.currentState.save();
+                Navigator.pushNamed(context, LoginSuccessScreen.routeName);
+              }
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (newValue) {
+        email = newValue;
+      },
+      onChanged: (value) {
+        if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+          setState(() {
+            errors.remove(kEmailNullError);
+          });
+        } else if (emailValidatorRegExp.hasMatch(value) &&
+            errors.contains(kInvalidEmailError)) {
+          setState(() {
+            errors.remove(kInvalidEmailError);
+          });
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty && !errors.contains(kEmailNullError)) {
+          setState(() {
+            errors.add(kEmailNullError);
+          });
+          return "";
+        } else if (emailValidatorRegExp.hasMatch(value) &&
+            !errors.contains(kInvalidEmailError)) {
+          setState(() {
+            errors.add(kInvalidEmailError);
+          });
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Email",
+        hintText: "Enter your emails",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(
+          svgIcon: "assets/icons/Mail.svg",
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      obscureText: true,
+      onSaved: (newValue) {
+        password = newValue;
+      },
+      onChanged: (value) {
+        if (value.isNotEmpty && errors.contains(kPassNullError)) {
+          setState(() {
+            errors.remove(kPassNullError);
+          });
+        } else if (value.length > 8 && errors.contains(kShortPassErrr)) {
+          setState(() {
+            errors.remove(kShortPassErrr);
+          });
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty && !errors.contains(kPassNullError)) {
+          setState(() {
+            errors.add(kPassNullError);
+          });
+          return "";
+        } else if (value.length < 8 && !errors.contains(kShortPassErrr)) {
+          setState(() {
+            errors.add(kShortPassErrr);
+          });
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Password",
+        hintText: "Enter your password",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: CustomSurffixIcon(
+          svgIcon: "assets/icons/Lock.svg",
+        ),
+      ),
+    );
+  }
+}
