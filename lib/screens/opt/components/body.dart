fc16d5ec48db3134852148c9eed856df0f4a060f
Donovan Avila <donovan.avila@gmail.com>
2021-07-20 10:28:08 -0500

    update view opt

--- lib/screens/opt/components/body.dart
+++ lib/screens/opt/components/body.dart
@@ -1,8 +1,170 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/components/default_button.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                "OTP Verification",
+                style: headingStyle,
+              ),
+              Text("We sent your code to +1 989 888**"),
+              buildTime(),
+              SizedBox(height: SizeConfig.screenHeight * 0.15),
+              OptForm(),
+              SizedBox(height: SizeConfig.screenHeight * 0.1),
+              GestureDetector(
+                onTap: () {},
+                child: Text("Resend OPT code",
+                    style: TextStyle(decoration: TextDecoration.underline)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row buildTime() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("This code will expired in "),
+        TweenAnimationBuilder(
+          tween: Tween(begin: 30.0, end: 0),
+          duration: Duration(seconds: 30),
+          builder: (context, value, child) => Text(
+            "00:${value.toInt()}",
+            style: TextStyle(color: kPrimaryColor),
+          ),
+          onEnd: () {},
+        )
+      ],
+    );
+  }
+}
+
+class OptForm extends StatefulWidget {
+  @override
+  _OptFormState createState() => _OptFormState();
+}
+
+class _OptFormState extends State<OptForm> {
+  FocusNode pin2FocusNode;
+  FocusNode pin3FocusNode;
+  FocusNode pin4FocusNode;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pin2FocusNode = FocusNode();
+    pin3FocusNode = FocusNode();
+    pin4FocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    pin2FocusNode.dispose();
+    pin3FocusNode.dispose();
+    pin4FocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  void nextField({String value, FocusNode focus}) {
+    // TODO: implement setState
+    if (value.length == 1) {
+      focus.requestFocus();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: getProportionateScreenWidth(60),
+                child: TextFormField(
+                  autofocus: true,
+                  obscureText: true,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                  decoration: otpInputDecoration,
+                  onChanged: (value) {
+                    nextField(value: value, focus: pin2FocusNode);
+                  },
+                ),
+              ),
+              SizedBox(
+                width: getProportionateScreenWidth(60),
+                child: TextFormField(
+                  focusNode: pin2FocusNode,
+                  obscureText: true,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                  decoration: otpInputDecoration,
+                  onChanged: (value) {
+                    nextField(value: value, focus: pin3FocusNode);
+                  },
+                ),
+              ),
+              SizedBox(
+                width: getProportionateScreenWidth(60),
+                child: TextFormField(
+                  focusNode: pin3FocusNode,
+                  obscureText: true,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                  decoration: otpInputDecoration,
+                  onChanged: (value) {
+                    nextField(value: value, focus: pin4FocusNode);
+                  },
+                ),
+              ),
+              SizedBox(
+                width: getProportionateScreenWidth(60),
+                child: TextFormField(
+                  focusNode: pin4FocusNode,
+                  obscureText: true,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(fontSize: 24),
+                  textAlign: TextAlign.center,
+                  decoration: otpInputDecoration,
+                  onChanged: (value) {
+                    pin4FocusNode.unfocus();
+                  },
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: SizeConfig.screenHeight * 0.15,
+          ),
+          DefaultButton(
+            text: "Continue",
+            press: () {},
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/screens/opt/opt_screen.dart
+++ lib/screens/opt/opt_screen.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'components/body.dart';
+
 class OptScreen extends StatelessWidget {
   static String routeName = '/opt';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("OPT Verification"),
+      ),
+      body: Body(),
+    );
   }
 }
